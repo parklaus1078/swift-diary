6e6c2fb4af683e0c795f1b25e536b5c1ca6c121d
Geun Woo Park <parklaus1078@gmail.com>
2023-09-04 12:47:36 +0900

    Added basic design frames

---
+++ diary/DiaryCell.swift
@@ -0,0 +1,12 @@
+//
+//  DiaryCell.swift
+//  diary
+//
+//  Created by Geun Woo Park on 2023/09/04.
+//
+
+import UIKit
+
+class DiaryCell: UICollectionViewCell {
+    
+}
---
+++ diary/StarCell.swift
@@ -0,0 +1,12 @@
+//
+//  StarCell.swift
+//  diary
+//
+//  Created by Geun Woo Park on 2023/09/04.
+//
+
+import UIKit
+
+class StarCell: UICollectionViewCell {
+    
+}
